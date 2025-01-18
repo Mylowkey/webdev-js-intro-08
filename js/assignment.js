2b0b2330c6670e0f9764dd447396ea6b50932f0d
Mylowkey <eemq20@gmail.com>
2025-01-18 17:38:22 -0500

    understood shortest ways or methods to do operations and also how to finally add or link the innerText within the same function with no need of an extra function as I was doing before or trying at least

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,44 @@ const largestNumberElement = document.getElementById("largest-number");
 const averageNumberElement = document.getElementById("average-number");
 
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
+
+
+
+function findSmallestNumber() {
+    const smallest = Math.min(...myNumbers);
+    console.log(smallest);
+    smallestNumberElement.textContent = smallest;
+}
+
+function findLargestNumber() {
+    const largest = Math.max(...myNumbers);
+ console.log(largest);
+    largestNumberElement.textContent = largest;
+}
+
+function findAverageNumber() {
+    const average = myNumbers.reduce((acumulator, current) => acumulator + current, 0) / myNumbers.length;
+    console.log(average);
+    averageNumberElement.textContent = average;
+// let sum = 0;
+// for (let i=0 ; i<myNumbers.length ; i++){
+//     sum += myNumbers[i];
+// }
+// let average = sum / myNumbers.length;
+// return average;
+}
+
+
+function render(){
+    findSmallestNumber();
+    findLargestNumber();
+    findAverageNumber();
+}
+
+submissionBtn.addEventListener("click", function ()  {
+  render();
+});
+
+
+
+
--- js/index.js
+++ js/index.js
@@ -1,3 +1,43 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+
+//1  Basic loop
+// let array = [4, 5, 6, 7, 8, 9, 10];
+// for (let i=0 ; i<array.length ; i++) {
+//     console.log(array[i]);
+// }
+
+
+//2 For of loop
+// let arrays = [4, 5, 6, 7, 8, 9, 10];
+//  for ( let array of arrays){
+//      console.log(array);
+//  }
+
+// 3. ForEach loop
+
+// let array = [4, 5, 6, 7, 8, 9, 10];
+
+// array.forEach(element => {
+//     console.log(element);
+// })
+
+// 4. change value for of loop
+// let array = [4, 5, 6, 7, 8, 9, 10];
+// for (let element of array){
+//     element = 2;
+//     console.log(element);
+// }
+
+5. // Loop through an array using the Array.forEach() method and try assigning all the elements a new value. What happens?
+
+// let array = [4, 5, 6, 7, 8, 9, 10];
+// array.forEach(Element => {
+//     Element = 3;
+//     console.log(Element);
+// })
+
+
+
